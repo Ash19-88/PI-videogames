cd07e337e04640fa8f113840e04f4f378c9a1285
Ash19-88 <ayelenllampa88@gmail.com>
2023-03-27 02:49:16 -0300

    Update Home.jsx

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
 import Cards from "../Cards/Cards";
+import style from "../Home/Home.module.css";
 
 
 
 export default function Home(){
     return(
         <>
-        <div>
+        <div className={style.mainHome}>
             <NavBar />
             <Cards />
         </div>
