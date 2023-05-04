8c0c274c648b72b8f84fdaa3edea1f0f0d635e14
Ash19-88 <ayelenllampa88@gmail.com>
2023-05-03 21:43:43 -0300

    updating vercel url

--- api/src/app.js
+++ api/src/app.js
@@ -18,7 +18,7 @@ server.use((req, res, next) => {
   // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Origin",
-    "https://videogames-ashtech.vercel.app"
+    "https://pi-videogames-ash19-88.vercel.app/"
   );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
