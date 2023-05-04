2793d43cc5a8a92777e9f762370c366527295d26
Ash19-88 <ayelenllampa88@gmail.com>
2023-05-03 21:20:50 -0300

    updating

--- api/src/db.js
+++ api/src/db.js
@@ -4,10 +4,22 @@ const fs = require("fs");
 const path = require("path");
 // const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 const { DATABASE_URL } = process.env;
+const pgSsl = require("pg-ssl");
+
+pg.defaults.ssl = true;
+pg.ssl = true;
+pg.sslMode = pgSsl;
 
 const sequelize = new Sequelize(`${DATABASE_URL}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 const basename = path.basename(__filename);
--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,8 @@ import Detail from "./components/Details/Detail";
 import CreateVideogame from "./components/Form/CreateVideogame";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001";
-// axios.defaults.baseURL = '';
+// axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://videogames-klev.onrender.com";
 
 function App() {
   return (
